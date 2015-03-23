b6f08bc330b41d5c40aad9f470ab9fc21d6443fd
Weilon <weilony@hotmail.com>
2015-03-23 11:55:22 +1100

    0.1 Alpha
    getFinalCost function now returns a float.
    Added getConcessionStatus function, to find out if the user is using a
    concession Opal card.
    Added #define's for concession types, and concession discount.

--- opalCalc.c
+++ opalCalc.c
@@ -12,23 +12,66 @@
  *  responsibility for mistakes in the calculation of the 
  *  journey fare (please don't sue me D:).
  */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
+#define NOT_CONCESSION 0
+#define CHILD_STUDENT_CONCESSION 1
+#define PENSIONER_CONCESSION 2
+
+#define CONCESSION_HALF_PRICE 0.5
+
 /* BEGIN DECLARATION OF FUNCTIONS */
 int getFinalCost (void);
+int getConcessionStatus (void);
 /* END DECLARATION OF FUNCTIONS */
 
 int main (int argc, char * argv[]) {
-   int costOfJourney = 0;
-
-   printf("The journey of your cost is: %d\n", costOfJourney);
+   float costOfJourney = 0;
+   
+   costOfJourney = getFinalCost();
+   
+   printf("The journey of your cost is: %.2f\n", costOfJourney);
    return EXIT_SUCCESS;
 }
 
-int getFinalCost (void) {
-	int finalCost = 0; //Initialise final cost variable
-
-	int finalCost = 10; //temporary setting to test main function.
+float getFinalCost (void) {
+    //Initialise return variable
+	float finalCost = 0; //Initialise final cost variable
+    
+    //Initialise calculation variables
+    int concessionStatus = NOT_CONCESSION; //Initialise as 0, so 
+    int isSunday = FALSE;
+    int basicCost = 0;
+    
+    //get value of concessionStatus variable
+    concessionStatus = getConcessionStatus();
+    
+	finalCost = 10; //temporary setting to test main function.
+    if (concessionStatus >= NOT_CONCESSION) {
+        finalCost = finalCost * CONCESSION_HALF_PRICE;
+    }
+    
 	return finalCost;
+}
+
+int getConcessionStatus (void) {
+    int inputConcessionStatus = NOT_CONCESSION;
+    
+    printf ("Are you using a concession Opal card?\n");
+    printf ("Enter 0 if you are not using a concession card, \n");
+    printf ("1 if you are using a child or student card and \n");
+    printf ("2 if you are using a pensioner card.\n");
+    printf ("Entering a value other than 0, 1 or 2 will crash the program.\n\n");
+    
+    scanf ("%d", &inputConcessionStatus);
+    
+    assert (inputConcessionStatus >= NOT_CONCESSION
+        && inputConcessionStatus <= PENSIONER_CONCESSION);
+    
+    return inputConcessionStatus;
 }
